2b63963528166193baafe0cc12aaf576749a3eb4
hokin2023 <hokin990@gmail.com>
2023-07-26 15:28:30 -0700

    fixed app js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb:127.0.0.1:27017/googlebooks', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -8,3 +8,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleboo
 });
 
 module.exports = mongoose.connection;
+// 127.0.0.1:27017
